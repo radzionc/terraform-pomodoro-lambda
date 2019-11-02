535ea199787ab52b631b5736cc06718e28bc8b8f
Rodion Chachura <geekrodion@gmail.com>
2019-11-02 11:35:38 +0300

    attempt: use indexes in with_domain.tf

--- with_domain.tf
+++ with_domain.tf
@@ -7,13 +7,13 @@ resource "aws_api_gateway_domain_name" "api" {
 resource "aws_route53_record" "api" {
   count = "${var.main_domain != "" ? 1 : 0}"
 
-  name    = "${aws_api_gateway_domain_name.api.domain_name}"
+  name    = "${aws_api_gateway_domain_name.api[0].domain_name}"
   type    = "A"
   zone_id = "${var.zone_id}"
 
   alias {
     evaluate_target_health = true
-    name                   = "${aws_api_gateway_domain_name.api.cloudfront_domain_name}"
+    name                   = "${aws_api_gateway_domain_name.api[0].cloudfront_domain_name}"
     zone_id                = "${var.zone_id}"
   }
 }
@@ -21,6 +21,6 @@ resource "aws_api_gateway_base_path_mapping" "api" {
   count = "${var.main_domain != "" ? 1 : 0}"
 
   api_id      = "${aws_api_gateway_rest_api.api.id}"
-  stage_name  = "${aws_api_gateway_deployment.api.stage_name}"
-  domain_name = "${aws_api_gateway_domain_name.api.domain_name}"
+  stage_name  = "${aws_api_gateway_deployment.api[0].stage_name}"
+  domain_name = "${aws_api_gateway_domain_name.api[0].domain_name}"
 }
