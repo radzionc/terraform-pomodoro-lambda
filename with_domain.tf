6987021281466a0c2422cef573fb9b54cb1cc433
Rodion Chachura <geekrodion@gmail.com>
2019-11-02 13:22:03 +0300

    fix: zone id

--- with_domain.tf
+++ with_domain.tf
@@ -14,7 +14,7 @@ resource "aws_route53_record" "api" {
   alias {
     evaluate_target_health = true
     name                   = "${aws_api_gateway_domain_name.api[0].cloudfront_domain_name}"
-    zone_id                = "${var.zone_id}"
+    zone_id                = "${aws_api_gateway_domain_name.api[0].cloudfront_zone_id}"
   }
 }
 resource "aws_api_gateway_base_path_mapping" "api" {
