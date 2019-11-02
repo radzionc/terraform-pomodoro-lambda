933edee6726a8121aaae77e662cfeec96d2893f1
Rodion Chachura <geekrodion@gmail.com>
2019-11-02 11:49:16 +0300

    fix: indexes in cd.tf

--- cd.tf
+++ cd.tf
@@ -29,7 +29,7 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
   count = "${var.ci_container_name != "" ? 1 : 0}"
 
   name = "tf-${var.name}-pipeline"
-  role = "${aws_iam_role.codepipeline_role.id}"
+  role = "${aws_iam_role.codepipeline_role[0].id}"
   policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -67,17 +67,17 @@ EOF
 resource "aws_iam_role_policy" "codebuild_policy" {
   count = "${var.ci_container_name != "" ? 1 : 0}"
 
-  role        = "${aws_iam_role.codebuild_role.name}"
+  role   = "${aws_iam_role.codebuild_role[0].name}"
   policy = <<POLICY
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "*",
-            "Resource": "*"
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*"
+    }
+  ]
 }
 POLICY
 }
@@ -86,13 +86,13 @@ resource "aws_codebuild_project" "codebuild" {
   count = "${var.ci_container_name != "" ? 1 : 0}"
 
   name         = "tf-codebuild-${var.name}"
-  service_role = "${aws_iam_role.codebuild_role.arn}"
+  service_role = "${aws_iam_role.codebuild_role[0].arn}"
   artifacts {
     type = "CODEPIPELINE"
   }
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${ci_containers_storage_name}:${var.ci_container_name}"
+    image        = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${var.ci_containers_storage_name}:${var.ci_container_name}"
     type         = "LINUX_CONTAINER"
 
     environment_variable {
@@ -109,9 +109,9 @@ resource "aws_codepipeline" "pipeline" {
   count = "${var.ci_container_name != "" ? 1 : 0}"
 
   name     = "tf-${var.name}-pipeline"
-  role_arn = "${aws_iam_role.codepipeline_role.arn}"
+  role_arn = "${aws_iam_role.codepipeline_role[0].arn}"
   artifact_store {
-    location = "${aws_s3_bucket.artifacts.bucket}"
+    location = "${aws_s3_bucket.artifacts[0].bucket}"
     type     = "S3"
   }
   stage {
@@ -140,7 +140,7 @@ resource "aws_codepipeline" "pipeline" {
       input_artifacts = ["source"]
       version         = "1"
       configuration = {
-        ProjectName = "${aws_codebuild_project.codebuild.name}"
+        ProjectName = "${aws_codebuild_project.codebuild[0].name}"
       }
     }
   }
