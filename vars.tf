38c6a71161d67630f6ef091042a3059eafd88551
Rodion Chachura <geekrodion@gmail.com>
2019-11-03 06:13:06 +0300

    update: optional params

--- vars.tf
+++ vars.tf
@@ -16,12 +16,26 @@ variable with_api_gateway {
 variable "ci_containers_storage_name" {
   default = "tf-ci"
 }
-variable "ci_container_name" {}
-variable "repo_owner" {}
-variable "repo_name" {}
-variable "branch" {}
+variable "ci_container_name" {
+  default = ""
+}
+variable "repo_owner" {
+  default = ""
+}
+variable "repo_name" {
+  default = ""
+}
+variable "branch" {
+  default = ""
+}
 
 // if you have a domain
-variable "main_domain" {}
-variable "zone_id" {}
-variable "certificate_arn" {}
+variable "main_domain" {
+  default = ""
+}
+variable "zone_id" {
+  default = ""
+}
+variable "certificate_arn" {
+  default = ""
+}
