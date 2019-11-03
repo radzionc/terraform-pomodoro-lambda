4e449a9f72a77cfc3ec4aa09d9f8e176ea75be7a
Rodion Chachura <geekrodion@gmail.com>
2019-11-03 06:10:41 +0300

    update: move module

--- gateway.tf
+++ gateway.tf
@@ -65,18 +65,65 @@ resource "aws_lambda_permission" "apigw" {
   source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api[0].id}/*/*"
 }
 
-module "cors" {
+// copied from: https://github.com/carrot/terraform-api-gateway-cors-module/blob/master/main.tf
+resource "aws_api_gateway_method" "resource_options" {
   count = "${var.with_api_gateway ? 1 : 0}"
 
-  source = "github.com/carrot/terraform-api-gateway-cors-module"
-  resource_id = "${aws_api_gateway_resource.api.id}"
   rest_api_id = "${aws_api_gateway_rest_api.api[0].id}"
+  resource_id = "${aws_api_gateway_resource.api[0].id}"
+  http_method = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "resource_options_integration" {
+  count = "${var.with_api_gateway ? 1 : 0}"
+
+  rest_api_id = "${aws_api_gateway_rest_api.api[0].id}"
+  resource_id = "${aws_api_gateway_resource.api[0].id}"
+  http_method = "${aws_api_gateway_method.resource_options[0].http_method}"
+  type = "MOCK"
+  request_templates = { 
+    "application/json" = <<PARAMS
+{ "statusCode": 200 }
+PARAMS
+  }
+}
+
+resource "aws_api_gateway_integration_response" "resource_options_integration_response" {
+  count = "${var.with_api_gateway ? 1 : 0}"
+
+  depends_on = ["aws_api_gateway_integration.resource_options_integration[0]"]
+  rest_api_id = "${aws_api_gateway_rest_api.api[0].id}"
+  resource_id = "${aws_api_gateway_resource.api[0].id}"
+  http_method = "${aws_api_gateway_method.resource_options[0].http_method}"
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
+resource "aws_api_gateway_method_response" "resource_options_200" {
+  count = "${var.with_api_gateway ? 1 : 0}"
+
+  depends_on = ["aws_api_gateway_method.resource_options[0]"]
+  rest_api_id = "${aws_api_gateway_rest_api.api[0].id}"
+  resource_id = "${aws_api_gateway_resource.api[0].id}"
+  http_method = "OPTIONS"
+  status_code = "200"
+  response_models = { "application/json" = "Empty" }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
 
 resource "aws_api_gateway_deployment" "api" {
   count = "${var.with_api_gateway ? 1 : 0}"
 
-  depends_on = ["module.cors[0]", "aws_api_gateway_integration.api[0]"]
+  depends_on = ["aws_api_gateway_integration_response.resource_options_integration_response[0]", "aws_api_gateway_integration.api[0]"]
   rest_api_id = "${aws_api_gateway_rest_api.api[0].id}"
   stage_name  = "${var.name}"
 }
