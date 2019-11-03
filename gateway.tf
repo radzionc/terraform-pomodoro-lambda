b86f0a5a04556a327c585bacf2078dccb74a9d7b
Rodion Chachura <geekrodion@gmail.com>
2019-11-03 05:59:04 +0300

    fix: typo

--- gateway.tf
+++ gateway.tf
@@ -18,7 +18,7 @@ resource "aws_api_gateway_integration" "api_root" {
 
   rest_api_id   = "${aws_api_gateway_rest_api.api[0].id}"
   resource_id   = "${aws_api_gateway_rest_api.api[0].root_resource_id}"
-  http_method = "${[aws_api_gateway_method.api_root[0].http_method}"
+  http_method = "${aws_api_gateway_method.api_root[0].http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
